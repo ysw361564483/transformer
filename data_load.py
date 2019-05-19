29c0e225847230beb7d9a29d132c773d916ef846
ysw361564483 <361564483@qq.com>
2019-05-19 12:07:56 +0800

    compare

--- data_load.py
+++ data_load.py
@@ -11,6 +11,8 @@ For example, fpath1, fpath2 means source file path and target file path, respect
 '''
 import tensorflow as tf
 from utils import calc_num_batches
+import pickle
+import os
 
 def load_vocab(vocab_fpath):
     '''Loads vocabulary file and returns idx<->token maps
@@ -21,9 +23,40 @@ def load_vocab(vocab_fpath):
     Returns
     two dictionaries.
     '''
+    # vocab = [line.split() for line in open(vocab_fpath, 'r').read().splitlines()]
+    # token2idx = {token: idx for token, idx in vocab}
+    # idx2token = {idx: token for token, idx in vocab}
+    dict_name = str(vocab_fpath).split('/')[-1].split('.')[0]+'.pkl'
+    print('Building ' +dict_name+ ' vocab')
+    if os.path.exists(dict_name):
+        with open(dict_name, 'rb') as f:
+            token2idx = pickle.load(f)
+            idx2token = pickle.load(f)
+        print('loading pkl vocab size =', len(idx2token), len(token2idx))
+        return token2idx, idx2token
+
     vocab = [line.split()[0] for line in open(vocab_fpath, 'r').read().splitlines()]
+    for i in ['</s>', '<s>', '<unk>', '<pad>']:
+        vocab.insert(0, i)
+    vocab = vocab[:10000]
     token2idx = {token: idx for idx, token in enumerate(vocab)}
     idx2token = {idx: token for idx, token in enumerate(vocab)}
+    # token2idx.update({'<s>':len(token2idx)})
+    # token2idx.update({'</s>':len(token2idx)})
+    # token2idx.update({'<pad>':len(token2idx)})
+    # token2idx.update({'<unk>':len(token2idx)})
+    # idx2token.update({len(idx2token): '<s>'})
+    # idx2token.update({len(idx2token): '<s>'})
+    # idx2token.update({len(idx2token): '<pad>'})
+    # idx2token.update({len(idx2token): '<unk>'})
+    print('vocab size =', len(idx2token), len(token2idx))
+    with open(dict_name, 'wb') as f:
+        pickle.dump(token2idx, f, True)
+        pickle.dump(idx2token, f, True)
+    with open('dict_'+str(vocab_fpath).split('/')[-1], 'w') as f:
+        for item in token2idx.items():
+            f.write(str(item))
+            f.write('\n')
     return token2idx, idx2token
 
 def load_data(fpath1, fpath2, maxlen1, maxlen2):
@@ -32,7 +65,7 @@ def load_data(fpath1, fpath2, maxlen1, maxlen2):
     fpath2: target file path. string.
     maxlen1: source sent maximum length. scalar.
     maxlen2: target sent maximum length. scalar.
-
+重大
     Returns
     sents1: list of source sents
     sents2: list of target sents
@@ -60,10 +93,11 @@ def encode(inp, type, dict):
     if type=="x": tokens = inp_str.split() + ["</s>"]
     else: tokens = ["<s>"] + inp_str.split() + ["</s>"]
 
-    x = [dict.get(t, dict["<unk>"]) for t in tokens]
+    x = [dict.get(t, dict.get('<unk>')) for t in tokens]
+    # print('len(x) =', len(x))
     return x
 
-def generator_fn(sents1, sents2, vocab_fpath):
+def generator_fn(sents1, sents2, vocab_fpath, vocab_fpath1):
     '''Generates training / evaluation data
     sents1: list of source sents
     sents2: list of target sents
@@ -80,16 +114,18 @@ def generator_fn(sents1, sents2, vocab_fpath):
         y_seqlen: int. sequence length of y
         sent2: str. target sentence
     '''
-    token2idx, _ = load_vocab(vocab_fpath)
+    q_token2idx, _ = load_vocab(vocab_fpath)
+    a_token2idx, _ = load_vocab(vocab_fpath1)
     for sent1, sent2 in zip(sents1, sents2):
-        x = encode(sent1, "x", token2idx)
-        y = encode(sent2, "y", token2idx)
+        x = encode(sent1, "x", q_token2idx)
+        y = encode(sent2, "y", a_token2idx)
+        
         decoder_input, y = y[:-1], y[1:]
 
         x_seqlen, y_seqlen = len(x), len(y)
         yield (x, x_seqlen, sent1), (decoder_input, y, y_seqlen, sent2)
 
-def input_fn(sents1, sents2, vocab_fpath, batch_size, shuffle=False):
+def input_fn(sents1, sents2, vocab_fpath, vocab_fpath1, batch_size, shuffle=False):
     '''Batchify data
     sents1: list of source sents
     sents2: list of target sents
@@ -119,17 +155,17 @@ def input_fn(sents1, sents2, vocab_fpath, batch_size, shuffle=False):
         generator_fn,
         output_shapes=shapes,
         output_types=types,
-        args=(sents1, sents2, vocab_fpath))  # <- arguments for generator_fn. converted to np string arrays
+        args=(sents1, sents2, vocab_fpath, vocab_fpath1))  # <- arguments for generator_fn. converted to np string arrays
 
     if shuffle: # for training
         dataset = dataset.shuffle(128*batch_size)
 
     dataset = dataset.repeat()  # iterate forever
     dataset = dataset.padded_batch(batch_size, shapes, paddings).prefetch(1)
-
+    # If the dimension is unknown (e.g. tf.Dimension(None)), the component will be padded out to the maximum length of all elements in that dimension.
     return dataset
 
-def get_batch(fpath1, fpath2, maxlen1, maxlen2, vocab_fpath, batch_size, shuffle=False):
+def get_batch(fpath1, fpath2, maxlen1, maxlen2, vocab_fpath, vocab_fpath1, batch_size, shuffle=False):
     '''Gets training / evaluation mini-batches
     fpath1: source file path. string.
     fpath2: target file path. string.
@@ -145,6 +181,6 @@ def get_batch(fpath1, fpath2, maxlen1, maxlen2, vocab_fpath, batch_size, shuffle
     num_samples
     '''
     sents1, sents2 = load_data(fpath1, fpath2, maxlen1, maxlen2)
-    batches = input_fn(sents1, sents2, vocab_fpath, batch_size, shuffle=shuffle)
+    batches = input_fn(sents1, sents2, vocab_fpath, vocab_fpath1, batch_size, shuffle=shuffle)
     num_batches = calc_num_batches(len(sents1), batch_size)
     return batches, num_batches, len(sents1)
--- train.py
+++ train.py
@@ -18,26 +18,28 @@ import logging
 
 logging.basicConfig(level=logging.INFO)
 
-
 logging.info("# hparams")
 hparams = Hparams()
 parser = hparams.parser
 hp = parser.parse_args()
+if not os.path.exists(hp.logdir): os.makedirs(hp.logdir)
 save_hparams(hp, hp.logdir)
 
 logging.info("# Prepare train/eval batches")
 train_batches, num_train_batches, num_train_samples = get_batch(hp.train1, hp.train2,
                                              hp.maxlen1, hp.maxlen2,
-                                             hp.vocab, hp.batch_size,
+                                             hp.vocab, hp.vocab1, hp.batch_size,
                                              shuffle=True)
 eval_batches, num_eval_batches, num_eval_samples = get_batch(hp.eval1, hp.eval2,
                                              100000, 100000,
-                                             hp.vocab, hp.batch_size,
+                                             hp.vocab, hp.vocab1, hp.batch_size,
                                              shuffle=False)
 
 # create a iterator of the correct shape and type
 iter = tf.data.Iterator.from_structure(train_batches.output_types, train_batches.output_shapes)
 xs, ys = iter.get_next()
+# print('x0, x1 =', xs[0].shape, x[1].shape )
+
 
 train_init_op = iter.make_initializer(train_batches)
 eval_init_op = iter.make_initializer(eval_batches)
@@ -45,7 +47,7 @@ eval_init_op = iter.make_initializer(eval_batches)
 logging.info("# Load model")
 m = Transformer(hp)
 loss, train_op, global_step, train_summaries = m.train(xs, ys)
-y_hat, eval_summaries = m.eval(xs, ys)
+y_hat, eval_summaries, eval_loss = m.eval(xs, ys)
 # y_hat = m.infer(xs, ys)
 
 logging.info("# Session")
@@ -62,40 +64,48 @@ with tf.Session() as sess:
     summary_writer = tf.summary.FileWriter(hp.logdir, sess.graph)
 
     sess.run(train_init_op)
+    # xx, yy = sess.run([xs, ys])
+    # print('xx[-1] =', xx[-1], 'xx[0] =', xx[0], sep='\n')
+    # print('yy[-1] =', yy[-1], 'yy[0]= ', yy[0], sep='\n')
+
     total_steps = hp.num_epochs * num_train_batches
     _gs = sess.run(global_step)
     for i in tqdm(range(_gs, total_steps+1)):
-        _, _gs, _summary = sess.run([train_op, global_step, train_summaries])
+        _, _gs, _summary, loss_ = sess.run([train_op, global_step, train_summaries, loss])
         epoch = math.ceil(_gs / num_train_batches)
         summary_writer.add_summary(_summary, _gs)
-
+        if _gs and _gs%500 == 0:
+            print('_gs{}/{}, loss= {}'.format(_gs, num_train_batches, loss_))
         if _gs and _gs % num_train_batches == 0:
             logging.info("epoch {} is done".format(epoch))
             _loss = sess.run(loss) # train loss
+            logging.info("loss = {}".format(_loss))
 
             logging.info("# test evaluation")
-            _, _eval_summaries = sess.run([eval_init_op, eval_summaries])
+            _, _eval_summaries, eval_loss_ = sess.run([eval_init_op, eval_summaries, eval_loss])
+            # print('eval xx[-1] =', xx[-1], 'xx[0] =', xx[0], sep='\n')
+            # print('eval yy[-1] =', yy[-1], 'yy[0]= ', yy[0], sep='\n')
             summary_writer.add_summary(_eval_summaries, _gs)
-
-            logging.info("# get hypotheses")
+            logging.info("evaluation loss = {}".format(eval_loss_))
+            # logging.info("# get hypotheses")
             hypotheses = get_hypotheses(num_eval_batches, num_eval_samples, sess, y_hat, m.idx2token)
 
             logging.info("# write results")
-            model_output = "iwslt2016_E%02dL%.2f" % (epoch, _loss)
+            model_output = "MSParS_E%02dL%.2f" % (epoch, _loss)
             if not os.path.exists(hp.evaldir): os.makedirs(hp.evaldir)
             translation = os.path.join(hp.evaldir, model_output)
             with open(translation, 'w') as fout:
                 fout.write("\n".join(hypotheses))
 
-            logging.info("# calc bleu score and append it to translation")
+            # logging.info("# calc bleu score and append it to translation")
             calc_bleu(hp.eval3, translation)
 
-            logging.info("# save models")
+            # logging.info("# save models")
             ckpt_name = os.path.join(hp.logdir, model_output)
             saver.save(sess, ckpt_name, global_step=_gs)
             logging.info("after training of {} epochs, {} has been saved.".format(epoch, ckpt_name))
 
-            logging.info("# fall back to train mode")
+            # logging.info("# fall back to train mode")
             sess.run(train_init_op)
     summary_writer.close()
 
